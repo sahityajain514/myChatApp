b5b4c5a7f649c46007891a9f5c0231ce79d16341
Sahitya <Sahityajain514@gmail.com>
2024-07-14 10:11:52 +0530

    for deploy

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -12,10 +12,10 @@ export const SocketContextProvider = ({ children }) => {
 	const [socket, setSocket] = useState(null);
 	const [onlineUsers, setOnlineUsers] = useState([]);
 	const { authUser } = useAuthContext();
-
+//https://chat-app-yt.onrender.com
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
